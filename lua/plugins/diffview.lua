c35182ee7cdbe40f47fe6d20bdff4639eda9c098
KP <109694228@qq.com>
2024-01-30 16:16:30 +0800

    Update keymaps for git commit and restore in diffview.

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -14,6 +14,37 @@ local function get_absolute_path_in_preview()
   return require("diffview.lib").get_current_view().cur_entry.absolute_path
 end
 
+local function get_visual_selection_range()
+  local start_pos = vim.fn.getpos("v")[2]
+  local end_pos = vim.fn.getpos(".")[2]
+  if end_pos < start_pos then
+    start_pos, end_pos = end_pos, start_pos
+  end
+  return start_pos, end_pos
+end
+
+local function get_item_from_line_number(line)
+  local comp = require("diffview.lib").get_current_view().panel.components.comp:get_comp_on_line(line)
+  if comp and comp.name == "file" then
+    return comp.context
+  elseif comp and comp.name == "dir_name" then
+    return comp.parent.context
+  end
+end
+
+local function get_visual_selection_paths()
+  local start_pos, end_pos = get_visual_selection_range()
+  local selected_items = {}
+  while start_pos <= end_pos do
+    local item = get_item_from_line_number(start_pos)
+    if item then
+      table.insert(selected_items, item.path)
+    end
+    start_pos = start_pos + 1
+  end
+  return selected_items
+end
+
 return {
   "sindrets/diffview.nvim",
   dependencies = {
@@ -54,31 +85,60 @@ return {
           { desc = "Git Commit" },
         },
         {
-          "n",
+          { "n", "v" },
           "A",
           function()
-            local results = vim.system({ "git", "add", get_path_at_cursor() }, { text = true }):wait()
-            if results.code ~= 0 then
-              vim.notify(
-                "Add failed with the message: \n" .. vim.trim(results.stdout .. "\n" .. results.stderr),
-                vim.log.levels.ERROR,
-                { title = "Add" }
-              )
+            local mode = vim.fn.mode()
+
+            local items
+            if mode == "n" then
+              items = { get_path_at_cursor() }
+            else
+              items = get_visual_selection_paths()
+            end
+
+            for _, item in ipairs(items) do
+              local results = vim.system({ "git", "add", item }, { text = true }):wait()
+              if results.code ~= 0 then
+                vim.notify(
+                  "Add failed with the message: \n" .. vim.trim(results.stdout .. "\n" .. results.stderr),
+                  vim.log.levels.ERROR,
+                  { title = "Add" }
+                )
+              end
+            end
+
+            if mode == "v" or mode == "V" then
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", false, true, true), "nx", false)
             end
           end,
           { desc = "Git Add" },
         },
         {
-          "n",
+          { "n", "v" },
           "D",
           function()
-            local results = vim.system({ "git", "restore", "--staged", get_path_at_cursor() }, { text = true }):wait()
-            if results.code ~= 0 then
-              vim.notify(
-                "Restore failed with the message: \n" .. vim.trim(results.stdout .. "\n" .. results.stderr),
-                vim.log.levels.ERROR,
-                { title = "Restore" }
-              )
+            local mode = vim.fn.mode()
+
+            local items
+            if mode == "n" then
+              items = { get_path_at_cursor() }
+            else
+              items = get_visual_selection_paths()
+            end
+
+            for _, item in ipairs(items) do
+              local results = vim.system({ "git", "restore", "--staged", item }, { text = true }):wait()
+              if results.code ~= 0 then
+                vim.notify(
+                  "Restore failed with the message: \n" .. vim.trim(results.stdout .. "\n" .. results.stderr),
+                  vim.log.levels.ERROR,
+                  { title = "Restore" }
+                )
+              end
+            end
+            if mode == "v" or mode == "V" then
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", false, true, true), "nx", false)
             end
           end,
           { desc = "Git Restore" },
