d941e9f1e1f71ae9def2b1f0aac270a8b46a32fc
KP <109694228@qq.com>
2024-04-19 14:54:58 +0800

    Update colorscheme config.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,43 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
   { "folke/tokyonight.nvim" },
-  { "EdenEast/nightfox.nvim" },
+  {
+    "EdenEast/nightfox.nvim",
+    opts = {
+      options = {
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        },
+      },
+    },
+  },
   { "shaunsingh/nord.nvim" },
   {
     "rose-pine/neovim",
     as = "rose-pine",
   },
-  { { "catppuccin/nvim", as = "catppuccin" } },
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        highlight_overrides = {
+          all = function(colors)
+            return {
+              DiagnosticHint = { fg = colors.teal, style = { "bold" } },
+              DiagnosticInfo = { fg = colors.sky, style = { "bold" } },
+              DiagnosticWarn = { fg = colors.yellow, style = { "bold" } },
+              DiagnosticError = { fg = colors.red, style = { "bold" } },
+            }
+          end,
+        },
+      })
+    end,
+  },
   { "NTBBloodbath/sweetie.nvim" },
-  { { "ramojus/mellifluous.nvim", dependencies = "rktjmp/lush.nvim" } },
+  { "ramojus/mellifluous.nvim", dependencies = "rktjmp/lush.nvim" },
   { "JoosepAlviste/palenightfall.nvim" },
   { "Mofiqul/dracula.nvim" },
   { "olimorris/onedarkpro.nvim" },
