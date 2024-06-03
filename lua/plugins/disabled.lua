5ec4b2fc9ef108d8590afe78782010ff15fee811
KP <109694228@qq.com>
2024-06-03 14:40:57 +0800

    Replace neodev with lazydev.

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,11 +1,12 @@
 return {
-  { "stevearc/aerial.nvim", enabled = false },
   { "anuvyklack/windows.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
+  { "folke/neodev.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "hiphish/rainbow-delimiters.nvim", enabled = false },
   { "nvimdev/lspsaga.nvim", enabled = false },
   { "rasulomaroff/reactive.nvim", enabled = false },
   { "simrat39/symbols-outline.nvim", enabled = false },
+  { "stevearc/aerial.nvim", enabled = false },
   { "tris203/precognition.nvim", enabled = false },
 }
