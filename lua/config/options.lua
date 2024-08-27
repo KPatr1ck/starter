b83aa0be5efb5684de787189f1dfff122caafe4d
KP <109694228@qq.com>
2024-08-27 12:15:59 +0800

    Add basedpyright for python inlay hints.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -120,3 +120,9 @@ vim.o.pumheight = 10
 vim.g.autoformat = true
 
 vim.o.conceallevel = 0
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "basedpyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+-- vim.g.lazyvim_python_ruff = "ruff"
