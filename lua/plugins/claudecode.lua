45c2437701769b682347d8db2e0b662056f2d69f
KP <109694228@qq.com>
2026-01-21 17:45:06 +0800

    feat: add claudecode.nvim

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,30 @@
+return {
+  "coder/claudecode.nvim",
+  opts = {
+    -- Diff Integration
+    diff_opts = {
+      auto_close_on_accept = true,
+      vertical_split = false,
+      open_in_current_tab = false,
+      keep_terminal_focus = true, -- If true, moves focus back to terminal after diff opens
+    },
+  },
+  keys = {
+    { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+    { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+    { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
+    { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
+    { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+    { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+    { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+    {
+      "<leader>as",
+      "<cmd>ClaudeCodeTreeAdd<cr>",
+      desc = "Add file",
+      ft = { "NvimTree", "neo-tree", "oil" },
+    },
+    -- Diff management
+    { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+    { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+  },
+}
